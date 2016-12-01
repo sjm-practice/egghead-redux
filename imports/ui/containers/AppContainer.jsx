7f1499ab99db45c7a493c31bb9a4959922be69e4
stevenjmarsh <sjm.atl@gmail.com>
2016-12-01 17:04:50 -0500

    switching to react (adding react packages, displaying initial Component)

---
+++ imports/ui/containers/AppContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Here we go!</h1>
+      </div>
+    );
+  }
+}
