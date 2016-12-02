6355e355198fed5e95bfee20f700fff67334e8e1
stevenjmarsh <sjm.atl@gmail.com>
2016-12-02 15:44:45 -0500

    adding eslint

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 
-import AppContainer from "../imports/ui/containers/AppContainer";
+import AppContainer from "../imports/ui/containers/AppContainer.jsx";
 
 Meteor.startup(() => {
   render(<AppContainer />, document.getElementById("render-target"));
