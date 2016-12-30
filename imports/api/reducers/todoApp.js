f9c92f016c11b15b1f3ecee0df379eba239f484d
stevenjmarsh <sjm.atl@gmail.com>
2016-12-30 17:34:18 -0500

    now using combineReducers to make composite instead of combining by hand

--- imports/api/reducers/todoApp.js
+++ imports/api/reducers/todoApp.js
@@ -1,9 +1,18 @@
+import { combineReducers } from "redux";
 import todos from "./todos";
 import visibilityFilter from "./visibilityFilter";
 
-const todoApp = (state = {}, action) => ({
-  todos: todos(state.todos, action),
-  visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-});
+// combine reducer written by hand... (note a lot of es6 shorthand)
+// const todoApp = (state = {}, action) => ({
+//   todos: todos(state.todos, action),
+//   visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+// });
+
+// using provided function...
+const todoApp = combineReducers({ todos, visibilityFilter });
+
+// NOTE: a useful convention is to name the reducers after the state key they manage
+//       ie, todos, visibilityFilter, allowing you to use es6 object literal
+//       shorthand notation above
 
 export default todoApp;
